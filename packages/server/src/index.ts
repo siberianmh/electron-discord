08c54bb2a1f6a4f7a11bdd2e4c23d9f9680c9010
vhashimotoo <hashimoto.stream@gmail.com>
2021-03-19 23:41:33 +0200

    add sentry to server
    GitOrigin-RevId: bf57049daadf7b8d50a4913901b65095c292814f

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata'
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import * as Sentry from '@sentry/node'
+import * as Tracing from '@sentry/tracing'
 
 import { connectMySQL } from './lib/connect-mysql'
 import { apiRoutes } from './api'
@@ -8,10 +10,25 @@ import { apiRoutes } from './api'
 const app = express()
 const port = process.env.PORT || 5000
 
+Sentry.init({
+  dsn:
+    'https://a22da8923d5f4ea7875fa8518335410b@o102026.ingest.sentry.io/5474186',
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+})
+
+app.use(Sentry.Handlers.requestHandler())
+app.use(Sentry.Handlers.tracingHandler())
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', apiRoutes)
+
+app.use(Sentry.Handlers.errorHandler())
 
 connectMySQL().then(() => {
   app.listen(port, () => {
