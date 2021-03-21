a9ef599c312fa889a65e790e6ceec619209d7f4d
vhashimotoo <hashimoto.stream@gmail.com>
2021-03-21 07:35:21 +0200

    disable sentry in the other way
    GitOrigin-RevId: e6f6408c039b759a6a6fdbad506a150933fa5306

--- packages/bot/src/index.ts
+++ packages/bot/src/index.ts
@@ -25,13 +25,12 @@ import {
 } from './modules'
 import { client } from './lib/discord'
 
-if (process.env.NODE_ENV !== 'development') {
-  Sentry.init({
-    dsn:
-      'https://a22da8923d5f4ea7875fa8518335410b@o102026.ingest.sentry.io/5474186',
-    tracesSampleRate: 1.0,
-  })
-}
+Sentry.init({
+  dsn:
+    'https://a22da8923d5f4ea7875fa8518335410b@o102026.ingest.sentry.io/5474186',
+  enabled: process.env.NODE_ENV !== 'development',
+  tracesSampleRate: 1.0,
+})
 
 for (const mod of [
   DownloadModule,
