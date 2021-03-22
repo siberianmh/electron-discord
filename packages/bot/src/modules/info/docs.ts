379a77e62985253d335cfd79550f49017b52fd1b
vhashimotoo <hashimoto.stream@gmail.com>
2021-03-22 18:10:10 +0200

    add docs-new command
    GitOrigin-RevId: 835d1a5391db53dcbab674da2d1d1ccbf630f3c1

--- packages/bot/src/modules/info/docs.ts
+++ packages/bot/src/modules/info/docs.ts
@@ -8,20 +8,33 @@ import { $TSFixMe } from '../../lib/types'
 
 export class DocsModule extends ExtendedModule {
   private searchClient: SearchClient
+
+  private newSearchClient: SearchClient
+
   private apiIndex: SearchIndex
   private guidesIndex: SearchIndex
+
+  private newIndex: SearchIndex
 
   public constructor(client: CookiecordClient) {
     super(client)
 
     // I stole this from https://github.com/electron/algolia-indices/blob/master/demo.js#L31
     this.searchClient = algoliasearch(
+      'L9LD9GHGQJ',
+      '24e7e99910a15eb5d9d93531e5682370',
+    )
+
+    // I stole this from https://github.com/electron/electronjs.org/pull/5233
+    this.newSearchClient = algoliasearch(
       'BH4D9OD16A',
       'c9e8f898b3b32afe40f0a96637e7ea85',
     )
 
-    this.apiIndex = this.searchClient.initIndex('electronjs')
+    this.apiIndex = this.searchClient.initIndex('apis')
     this.guidesIndex = this.searchClient.initIndex('tutorials')
+
+    this.newIndex = this.newSearchClient.initIndex('electronjs')
   }
 
   private NOT_FOUND_EMBED = (searchIndex: string) =>
@@ -33,17 +46,16 @@ export class DocsModule extends ExtendedModule {
   public async docs(msg: Message, searchIndex: string) {
     const { hits } = await this.apiIndex.search(searchIndex)
 
-    console.log(hits)
-    // if (!hits.length) {
-    //   return msg.channel.send({ embed: this.NOT_FOUND_EMBED(searchIndex) })
-    // }
+    if (!hits.length) {
+      return msg.channel.send({ embed: this.NOT_FOUND_EMBED(searchIndex) })
+    }
 
-    // // Let's take the first result for now
-    // // TODO: Add Types maybe 😒
-    // const result: $TSFixMe = hits[0]
-    // const embed = this.createEmbed(result)
+    // Let's take the first result for now
+    // TODO: Add Types maybe 😒
+    const result: $TSFixMe = hits[0]
+    const embed = this.createEmbed(result)
 
-    // return createSelfDestructMessage(msg, embed)
+    return createSelfDestructMessage(msg, embed)
   }
 
   @extendedCommand({
@@ -63,6 +75,25 @@ export class DocsModule extends ExtendedModule {
     return createSelfDestructMessage(msg, embed)
   }
 
+  @extendedCommand({
+    aliases: ['docs-new', 'dd', 'dn'],
+    single: true,
+  })
+  public async docsd(msg: Message, searchIndex: string) {
+    const { hits } = await this.newIndex.search(searchIndex)
+
+    if (!hits.length) {
+      return msg.channel.send({ embed: this.NOT_FOUND_EMBED(searchIndex) })
+    }
+
+    const result: $TSFixMe = hits[0]
+
+    console.log(result)
+    const embed = this.createNewEmbed(result)
+
+    return createSelfDestructMessage(msg, embed)
+  }
+
   private createEmbed(result: $TSFixMe) {
     const title = result.fullSignature ?? result.title
     let description = result.description ?? result.body
@@ -71,6 +102,19 @@ export class DocsModule extends ExtendedModule {
       description =
         result.tldr ?? 'Unable to show preview because the body is too long.'
     }
+
+    const embed = new MessageEmbed()
+      .setTitle(title)
+      .setURL(result.url)
+      .setDescription(description)
+
+    return embed
+  }
+
+  private createNewEmbed(result: $TSFixMe) {
+    // NOTE: This probably should be the latest level in hierarchy
+    const title = result.anchor
+    const description = result.content ?? 'Unable to find description'
 
     const embed = new MessageEmbed()
       .setTitle(title)
