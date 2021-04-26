d4f6ce516bb33779966a6f5272d647fdf5366d08
vhashimotoo <hashimoto.stream@gmail.com>
2021-04-27 01:45:18 +0300

    chore: add buttowski
    GitOrigin-RevId: 35d5ffb4a7af4cc7bbc81afaaead5954ee6e5e76

---
+++ packages/buttowski/src/index.ts
@@ -0,0 +1,63 @@
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({
+    path: `${process.env.SIBERIAN_HOME}/.env` ?? undefined,
+  })
+}
+import { Client, Intents } from 'discord.js'
+
+const guildId =
+  process.env.NODE_ENV === 'development'
+    ? '715327188307804341'
+    : '745037351163527189'
+
+const unverefRole =
+  process.env.NODE_ENV === 'development'
+    ? '767473838090878976'
+    : '781303266562015242'
+
+const kickedMessage =
+  'Hello! You have been kicked from Electron Discord as you have failed to accept our rules. If this was an accident, please feel free to join us again!\n\nhttps://discord.gg/electron'
+
+const client = new Client({
+  ws: { intents: Intents.ALL },
+  partials: ['REACTION', 'MESSAGE', 'USER', 'CHANNEL'],
+  retryLimit: 3,
+})
+
+client.login(process.env.DISCORD_TOKEN)
+
+client.on('ready', async () => {
+  console.log(`Logged in as ${client.user!.tag}`)
+
+  const unverefMembers = (await client.guilds.fetch(guildId)).roles.cache
+    .get(unverefRole)
+    ?.members!.array()!
+
+  for (let i = 0; i < 25; i++) {
+    const member = unverefMembers[i]
+
+    if (!member) {
+      continue
+    }
+
+    console.log(`[${member.user.tag}] Started kicking of ${member.user.tag}`)
+    // We may fail this because user can have closed DMs
+    try {
+      console.log(`[${member.user.tag}] Sending DM about kicking`)
+      if (process.env.NODE_ENV !== 'development') {
+        await member.user.send(kickedMessage)
+      }
+    } catch (e) {
+      // Ignore, it isn't make sense to do something here.
+      console.log(`[${member.user.tag}] Unable to send DM 😐`)
+    } finally {
+      // But we anyway should kick them
+      console.log(`[${member.user.tag}] Kicking`)
+      if (process.env.NODE_ENV !== 'development') {
+        await member.kick('Failing accepting our rules.')
+      }
+    }
+  }
+
+  return process.exit(0)
+})
