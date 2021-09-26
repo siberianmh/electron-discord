44e39c0e00a3cd63b92eabd5856607d3ada885a0
sokrox <sokrox@outlook.com>
2021-09-26 04:59:39 +0300

    one more

--- packages/bot/test/lib/format-user.test.ts
+++ packages/bot/test/lib/format-user.test.ts
@@ -5,7 +5,6 @@ describe('formatUser', () => {
     const user = {
       id: '1345678900',
     }
-
     const result = formatUser(user as any)
     expect(result).toEqual('<@1345678900> (`1345678900`)')
   })
